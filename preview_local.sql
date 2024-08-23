0a1f75216634979375eb346d8bbf9b0b8c54a4ef
Minsait Ivan Gael Rojas Perez <nem.gael@coppel.com>
2024-08-23 11:49:18 -0600

    Update changesets

--- preview_local.sql
+++ preview_local.sql
@@ -6,7 +6,7 @@ GO
 -- Update Database Script
 -- *********************************************************************
 -- Change Log: changelog/master.xml
--- Ran at: 23/8/24, 11:31
+-- Ran at: 23/8/24, 11:37
 -- Against: sa@jdbc:sqlserver://localhost:1433;connectRetryInterval=10;connectRetryCount=1;maxResultBuffer=-1;sendTemporalDataTypesAsStringForBulkCopy=true;delayLoadingLobs=true;useFmtOnly=false;useBulkCopyForBatchInsert=false;cancelQueryTimeout=-1;sslProtocol=TLS;calcBigDecimalPrecision=false;useDefaultJaasConfig=false;jaasConfigurationName=SQLJDBCDriver;statementPoolingCacheSize=0;serverPreparedStatementDiscardThreshold=10;enablePrepareOnFirstPreparedStatementCall=false;fips=false;socketTimeout=0;authentication=NotSpecified;authenticationScheme=nativeAuthentication;xopenStates=false;datetimeParameterType=datetime2;sendTimeAsDatetime=true;replication=false;trustStoreType=JKS;trustServerCertificate=true;TransparentNetworkIPResolution=true;iPAddressPreference=IPv4First;serverNameAsACE=false;sendStringParametersAsUnicode=true;selectMethod=direct;responseBuffering=adaptive;queryTimeout=-1;packetSize=8000;multiSubnetFailover=false;loginTimeout=30;lockTimeout=-1;lastUpdateCount=true;useFlexibleCallableStatements=true;useDefaultGSSCredential=false;prepareMethod=prepexec;encrypt=true;disableStatementPooling=true;databaseName=dbLiquibase;columnEncryptionSetting=Disabled;applicationName=Microsoft JDBC Driver for SQL Server;applicationIntent=readwrite;
 -- Liquibase version: 4.29.1
 -- *********************************************************************
@@ -18,7 +18,7 @@ GO
 CREATE TABLE test_table (test_id int NOT NULL, test_column int, CONSTRAINT PK_TEST_TABLE PRIMARY KEY (test_id));
 GO
 
-INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('1', 'your.name', 'changelog/master.xml', GETDATE(), 1, '9:17d14650b22d67a4cec8954ac35d6a47', 'createTable tableName=test_table', '', 'EXECUTED', NULL, NULL, '4.29.1', '4434316108')
+INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('1', 'your.name', 'changelog/master.xml', GETDATE(), 1, '9:17d14650b22d67a4cec8954ac35d6a47', 'createTable tableName=test_table', '', 'EXECUTED', NULL, NULL, '4.29.1', '4434664074')
 GO
 
 -- Release Database Lock
