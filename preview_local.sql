549ed1b09304fdb445808d9f9f2b224c6adc0c56
Minsait Ivan Gael Rojas Perez <nem.gael@coppel.com>
2024-08-27 11:50:25 -0600

    Update database

--- preview_local.sql
+++
@@ -1,8 +0,0 @@
-
-UPDATE SUMMARY
-Run:                          1
-Previously run:               0
-Filtered out:                 0
--------------------------------
-Total change sets:            1
-
